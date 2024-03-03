730597b341ad54af68c01fb5705538a6762d9acb
donghoony <aru0504@naver.com>
2024-03-03 19:29:37 +0900

    feat: add emoji checkbox, beautify name input

--- js/donghoony.js
+++ js/donghoony.js
@@ -1,5 +1,17 @@
 document.getElementById("submitBtn").onclick = function() {
-    alertCheckedCount(getName(), getCount());
+    const name = getName();
+    
+    if (validateEmptyName(name)) {
+        alertCheckedCount(name, getCount());
+    }
+}
+
+function validateEmptyName(name) {
+    if (name.trim() == "") {
+        alert("이름을 입력해 주세요");
+        return false;
+    }
+    return true;
 }
 
 function getName() {
@@ -7,9 +19,13 @@ function getName() {
 }
 
 function getCount() {
-    return document.querySelectorAll(".song:checked").length;
+    return document.querySelectorAll(".vote:checked").length;
 }
 
 function alertCheckedCount(name, count) {
+    if (count <= 0) {
+        alert(name + "님은 저와 맞는 취향이 없는 것 같아요 🙄");
+        return;
+    }
     alert(name + "님, 저와 " + count + "개의 취향이 같으시네요!");
 }
